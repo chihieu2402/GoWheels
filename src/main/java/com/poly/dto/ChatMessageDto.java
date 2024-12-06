a38c7602d91747c7e25dd440ad9f628605fb432a
trantutai2014 <trantutai201204@gmail.com>
2024-12-06 21:00:09 +0700

    Chatbox

---
+++ src/main/java/com/poly/dto/ChatMessageDto.java
@@ -0,0 +1,17 @@
+package com.poly.dto;
+
+public class ChatMessageDto {
+    private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+   
+}
+
+
